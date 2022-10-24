87e6146531487c3898e6ef5a8387f174db3d9b95
khanhjoi <khanhb2003835@student.ctu.edu.vn>
2022-10-24 19:33:21 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, không thể tìm thấy trang. Trở về
+            <router-link to="/">Trang Chủ.</router-link>
+        </p>
+    </div>
+</template>
